b009bc4d532aab9a0e084d25d440bf3a5c056335
jjbb1221 <41380360+jjbb1221@users.noreply.github.com>
2018-07-26 00:31:17 -0500

    Delete alert.js

--- Commands/Public/alert.js
+++
@@ -1,8 +0,0 @@
-module.exports = (bot, db, config, winston, userDocument, serverDocument, channelDocument, memberDocument, msg, suffix) => {
-	let message = `**@${msg.author.username}** sent an alert in #${msg.channel.name} on ${msg.guild.name}`;
-	if(suffix) {
-		message += `:\`\`\`${suffix}\`\`\``;
-	}
-	bot.messageBotAdmins(msg.guild, serverDocument, message);
-	msg.channel.createMessage("I have alerted the admins.");
-};

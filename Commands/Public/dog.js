a2d419b9e77966000587513437cc158193791b42
jjbb1221 <41380360+jjbb1221@users.noreply.github.com>
2018-07-26 00:33:58 -0500

    Delete dog.js

--- Commands/Public/dog.js
+++
@@ -1,5 +0,0 @@
-const randomPuppy = require("random-puppy");
-
-module.exports = (bot, db, config, winston, userDocument, serverDocument, channelDocument, memberDocument, msg) => {
-	randomPuppy().then(msg.channel.createMessage);
-};

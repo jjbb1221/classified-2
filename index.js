3408a4e0c1065e632ec2890f3aae01337960050d
jjbb1221 <41380360+jjbb1221@users.noreply.github.com>
2018-07-26 07:54:27 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// Discord.js bot
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    client.user.setActivity('koshsystems.org/database', {type: 'WATCHING'});
+});
+
+client.on('message', msg => {
+    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
+    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
+    const args = msg.content.split(' ').slice(1).join(' ');
+    if (command === 'guide') return msg.channel.send('koshsystems.org/invite');
+    else if (command === 'invite') return msg.channel.send(process.env.INVITE);
+});
+
+client.login(process.env.TOKEN);

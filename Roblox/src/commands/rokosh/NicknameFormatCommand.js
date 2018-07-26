dc556e98a896feaffd21970c63cafd84a9bbe345
jjbb1221 <41380360+jjbb1221@users.noreply.github.com>
2018-07-25 22:56:01 -0500

    Create NicknameFormatCommand.js

---
+++ Roblox/src/commands/rokosh/NicknameFormatCommand.js
@@ -0,0 +1,34 @@
+const Command = require('../Command')
+
+module.exports =
+class NicknameFormatCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'nicknameformat',
+      properName: 'NicknameFormat',
+      aliases: ['rovernicknameformat'],
+      description: "`<format>` Set the format Kosh RoConnect will use when setting users' nicknames. Available replacements are %USERNAME%, %RANK%, %USERID%, %SERVER%, %DISCORDNAME%, and %DISCORDID%. Example:` %USERNAME% - (%USERID%)`. Default `%USERNAME%`.",
+
+      args: [
+        {
+          key: 'format',
+          label: 'format',
+          prompt: 'Nickname format',
+          type: 'string',
+          infinite: false,
+          default: false
+        }
+      ]
+    })
+  }
+
+  async fn (msg, args) {
+    if (args.format) {
+      this.server.setSetting('nicknameFormat', args.format)
+      msg.reply(`Set nickname format to \`${args.format}\``)
+    } else {
+      this.server.setSetting('nicknameFormat', undefined)
+      msg.reply('Nickname format set back to default')
+    }
+  }
+}
